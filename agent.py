05cbb2e80d9198f197d8369e733c8b467b81bf16
forgefront-ai <forgefront.ai@gmail.com>
2024-11-02 15:02:36 -0600

    Update agent.py

--- agent.py
+++ agent.py
@@ -3,6 +3,9 @@ import openai
 from config import API_KEY, MODEL, Assist_ID
 from openai import OpenAI
 import time
+import streamlit as st
+
+API_KEY = st.secrets['API_KEY']
 
 if API_KEY is None:
     raise ValueError("API_KEY no está configurada correctamente.")
