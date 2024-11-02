c843b86fac46c250d2d3380792e45ff22ffe0995
forgefront-ai <forgefront.ai@gmail.com>
2024-11-02 15:00:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 import streamlit as st
 
 
-headers = {
-  'API_KEY': st.secrets['API_KEY']
-}
+API_KEY = st.secrets['API_KEY']
 MODEL = "gpt-4o-mini"
 Assist_ID = 'asst_xVoZer5UU9I3ti8iN7t3IyRM'
