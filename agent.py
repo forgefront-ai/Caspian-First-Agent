f1944858c9c2ebbab43fc091c03703516c11f691
forgefront-ai <forgefront.ai@gmail.com>
2024-11-02 14:54:50 -0600

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,8 +4,8 @@ from config import API_KEY, MODEL, Assist_ID
 from openai import OpenAI
 import time
 
- if API_KEY is None:
-         raise ValueError("API_KEY no está configurada correctamente.")
+if API_KEY is None:
+    raise ValueError("API_KEY no está configurada correctamente.")
 
 class CaspianAgent:
     def __init__(self):
